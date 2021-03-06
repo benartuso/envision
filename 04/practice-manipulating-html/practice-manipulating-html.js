83b0d6586ab0ad45863d412130c1a2ae27f0cf7d
Ben Artuso <bja2mb@virginia.edu>
2021-03-06 15:23:45 -0500

    updating file structure, include practice files

--- 04/practice-manipulating-html/practice-manipulating-html.js
+++ 04/practice-manipulating-html/practice-manipulating-html.js
@@ -2,9 +2,12 @@
 
 //PART A: Change the text in the document's h1 element to "DOM manipulation: practice is going very well!"
 
+
 //PART B: Change the font of ALL the text in the linked HTML document to Helvetica. 
 
+
 //PART C: We've created a class in the attached stylsheet, style.css, called "night-mode" that will apply a very shitty night mode to this document's body. Bind the BUTTON below such that when it is clicked, it toggles our shitty night mode on and off.
+
 
 //PART D: Write some js such that when the div is hovered over, the font size increases to 32px temporarily. 
 
